1c7ed34335d4dc7e48b6d85b5ffd841dd07501a9
Lakshmi Satya Sai Sindhu Karri <lakarri@microsoft.com>
2024-06-14 07:27:27 +0000

    Give clustername and rgname to be used in a config file cluster_config.yaml

--- kube_stress.py
+++ kube_stress.py
@@ -8,7 +8,14 @@ import os
 credential = DefaultAzureCredential()
 container_service_client = ContainerServiceClient(credential, '60f3ac75-ce11-4deb-9030-d3896b73c46c')
 
-kubeconfig = container_service_client.managed_clusters.list_cluster_user_credentials("bhapathak-rg", "kubcluster_testing").kubeconfigs[0].value.decode("utf-8")
+rg_name = ""
+cluster_name = ""
+with open(os.path.join(os.path.dirname(__file__), "cluster_config.yaml")) as f:
+        cluster_config = yaml.safe_load(f)
+        rg_name = cluster_config["rg_name"]
+        cluster_name = cluster_config["cluster_name"]
+
+kubeconfig = container_service_client.managed_clusters.list_cluster_user_credentials(rg_name, cluster_name).kubeconfigs[0].value.decode("utf-8")
 cfg_dict = yaml.safe_load(kubeconfig)
 loader = KubeConfigLoader(cfg_dict)
 config = client.Configuration()
